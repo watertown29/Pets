e3c5450632d76a405487da069acdcf81177c3080
Daniel <djp297@cornell.edu>
2019-08-14 20:39:08 -0400

    Add content URI strings

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -1,12 +1,25 @@
 package com.example.android.pets.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public final class PetContract {
 
+    //empty constructor
     private PetContract(){}
 
+    //content authority from manifest
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+    //content string and content authority
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    //string of pets table
+    public static final String PATH_PETS = "pets";
+
     public static abstract class PetEntry implements BaseColumns {
+
+        /** The content URI to access the pet data in the provider */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
+
         public static final String TABLE_NAME = "pets";
         public static final String _ID = BaseColumns._ID;
         public static final String COLUMN_PET_NAME = "name";
@@ -20,5 +33,6 @@ public final class PetContract {
         public static final int GENDER_UNKNOWN = 0;
         public static final int GENDER_MALE = 1;
         public static final int GENDER_FEMALE = 2;
+
     }
 }
